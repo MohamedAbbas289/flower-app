61303a8e74b33c081ff177f44c39a958d85f2328
ahmedmohsen087 <ahmedmohsen0871@gmail.com>
2026-05-03 14:14:12 +0300

    add app theme and edit TextFormField and ElevatedButton in the theme app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flower_app/core/theme/app_theme.dart';
 import 'package:flower_app/core/utils/app_routes.dart';
 import 'package:flutter/material.dart';
 
@@ -16,6 +17,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: HomeScreen.routeName,
       routes: AppRoutes.routes,
+      theme: AppTheme.lightTheme,
     );
   }
 }
