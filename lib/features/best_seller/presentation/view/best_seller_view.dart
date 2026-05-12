9228b3432618b2274e7d881ea18b975ef9030f02
Abdelrazek Mahmoud <Aalrazek02@gmail.com>
2026-05-13 01:40:53 +0300

    feat: Add BestSellerView widget for displaying best seller products

---
+++ lib/features/best_seller/presentation/view/best_seller_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BestSellerView extends StatelessWidget {
+  const BestSellerView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
