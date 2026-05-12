2ef22ff23d71512646181b53835f79e3496f2ed0
Abdelrazek Mahmoud <Aalrazek02@gmail.com>
2026-05-13 01:39:04 +0300

    feat: Implement BestSellerApiClient and BestSellerRemoteDataSource for fetching best seller products

---
+++ lib/features/best_seller/api/api_client/best_seller_api_client.dart
@@ -0,0 +1,16 @@
+
+import 'package:dio/dio.dart';
+import 'package:flower_app/core/values/endpoints.dart';
+import 'package:flower_app/features/best_seller/data/model/best_seller_response.dart';
+import 'package:injectable/injectable.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'best_seller_api_client.g.dart';
+@lazySingleton
+@RestApi()
+abstract class BestSellerApiClient {
+  @factoryMethod
+  factory BestSellerApiClient(Dio dio) = _BestSellerApiClient;
+  @GET(Endpoints.bestSeller)
+  Future<BestSellerResponse> fetchBestSellers();
+}
