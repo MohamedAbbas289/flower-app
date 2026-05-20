19c11cd4b22a04184cb2a79eb3da0e9f9ebfa6d2
ahmedmohsen087 <ahmedmohsen0871@gmail.com>
2026-05-20 11:32:04 +0300

    add edit profile repo contract and impl  in dat data layer and domain layer .

--- lib/features/get_profile_screen/api/profile_api_client/get_profile_api_client.dart
+++ lib/features/get_profile_screen/api/profile_api_client/get_profile_api_client.dart
@@ -5,22 +5,17 @@ import 'package:retrofit/error_logger.dart';
 import 'package:retrofit/http.dart';
 import '../../../../core/values/endpoints.dart';
 import '../../data/models/get_profile_response.dart';
-import '../../data/models/get_user_dto.dart';
-part 'profile_api_client.g.dart';
+
+part 'get_profile_api_client.g.dart';
+
 @injectable
 @RestApi()
 abstract class ProfileApiClient {
   @factoryMethod
-  factory ProfileApiClient(Dio dio,) = _ProfileApiClient;
+  factory ProfileApiClient(Dio dio) = _ProfileApiClient;
 
 
   @GET(Endpoints.getProfile)
   Future<GetProfileResponse> getProfileData();
-
-  @PUT(Endpoints.editProfile)
-  Future<GetProfileResponse> updateProfile({
-    @Body() required GetUserDto request,
-  });
-
 
 }
