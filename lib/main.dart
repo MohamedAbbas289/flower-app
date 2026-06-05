4bfbcfcec7bb20b60654ae90c8095a42625d534c
Mohamed Abbas <89034968+MohamedAbbas289@users.noreply.github.com>
2026-06-05 15:14:22 +0300

    + Implement `FirebaseService` to encapsulate Firebase initialization and Crashlytics setup + Update `main.dart` to use `FirebaseService` for cleaner initialization logic + Configure Firebase Crashlytics and Google Services plugins in Android build files + Update `.gitignore` to exclude sensitive Firebase configuration files and generated options + Upgrade `google-services` dependency to version 4.4.4 and add `firebase-crashlytics-gradle` plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,14 @@
-import 'dart:ui';
-
 import 'package:easy_localization/easy_localization.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flower_app/config/auth/auth_manager.dart';
 import 'package:flower_app/config/di/di.dart';
+import 'package:flower_app/config/firebase/firebase_service.dart';
 import 'package:flower_app/my_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-import 'firebase_options.dart';
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // Initialize Firebase and Crashlytics
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  FlutterError.onError = (flutterErrorDetails) {
-    FirebaseCrashlytics.instance.recordFlutterFatalError(flutterErrorDetails);
-  };
-  PlatformDispatcher.instance.onError = (error, stack) {
-    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
-    return true;
-  };
+  await FirebaseService.initialize();
 
   await EasyLocalization.ensureInitialized();
   await configureDependencies();
