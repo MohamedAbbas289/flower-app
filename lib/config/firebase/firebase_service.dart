3a98e97c7523ca8e381bd4f3d1084917398484ee
AbdelRahman Shalan <abdelrahman.shalaan03@gmail.com>
2026-06-09 03:56:52 +0300

    fix: update GitHub Actions to create google-services.json and firebase_options.dart using heredoc syntax

--- lib/config/firebase/firebase_service.dart
+++ lib/config/firebase/firebase_service.dart
@@ -1,10 +1,9 @@
 import 'dart:ui';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flower_app/firebase_options.dart';
 import 'package:flutter/material.dart';
 
-import '../../firebase_options.dart';
 
 class FirebaseService {
   FirebaseService._();
